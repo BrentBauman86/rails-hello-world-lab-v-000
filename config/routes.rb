1161d8afb186d778c634262ec999f79e79473dd5
Brent Bauman <brentbauman86@gmail.com>
2018-10-11 00:19:26 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-get 'helloworld', to:
+get 'hello_world', to:
 'static#about'
   # Example of regular route:
     # get 'products/:id' => 'catalog#view'
